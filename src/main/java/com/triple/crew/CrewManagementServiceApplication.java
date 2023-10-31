eebf9187582457800aa815007db1350b07f8a0b5
LEYN <leyn53.triple@gmail.com>
2023-10-31 10:01:40 +0900

    Initial commit

---
+++ src/main/java/com/triple/crew/CrewManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.triple.crew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewManagementServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrewManagementServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/triple/crew/CrewManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.crew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewManagementServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
